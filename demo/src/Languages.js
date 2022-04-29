0790ab8f7884a0abb19d619776a433731a6352b0
Saule <SDossymbekova@one.kz>
2022-04-29 16:30:56 +0300

    lecture7: added Profiler memo example

---
+++ demo/src/Languages.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { memo } from "react";
+
+const Languages = ({ languages }) => {
+    return (
+        <div>
+            <h3>Top 6 Languages:</h3>
+            {(console.log("I am mounting!"),
+              languages.map((language, index) => <p key={index}> {language} </p>))
+            }
+        </div>
+    );
+};
+Languages.propTypes = {
+    languages : PropTypes.array.isRequired,
+};
+
+export default memo(Languages);
